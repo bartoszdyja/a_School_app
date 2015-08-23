1bdc98fb426ec468f009b4bc3608064d9499c143
railsbeginner <bartosz.dyja@gmail.com>
2015-08-23 15:26:35 +0200

    Refactor payment query

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   expose(:subject_items) { SubjectItem.includes(:teacher, :students) }
-  expose(:students)
+  expose(:payments) {Payment.includes(:student)}
 
   before_action :authenticate_user!, only: :payments
 end
